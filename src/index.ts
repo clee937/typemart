1a6a16faf3707d7ac9f0d963c85a0e6bb08b6f4e
Cheryl Lee <clee0937@gmail.com>
2025-03-27 17:16:17 +0000

    added array find method to locate the user's product in the products list

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 import products from "./products";
+
+const productName: string = "tote bag";
+
+// find the matching item from the inventory
+const product = products.find((item) => item.name === productName);
