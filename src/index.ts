0940a26d074f3fb9e6444a5f1465a18e510154b6
Cheryl Lee <clee0937@gmail.com>
2025-03-27 17:38:50 +0000

    added error handling for product not found

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import products from "./products";
 
 const productName: string = "shirt";
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
+let shippingAddress: string = "123 Maple Avenue, New York City, New York";
 
 // find the matching item from the inventory
 const product = products.find((item) => item.name === productName);
@@ -10,7 +15,11 @@ const product = products.filter((item) => item.name === productName)[0]; */
 
 console.log(`Found product ${JSON.stringify(product)}`);
 
-const preOrder = product?.preOrder.toLowerCase() === "true";
+if (!product) {
+  throw new Error("Product not found");
+}
+
+const preOrder = product.preOrder.toLowerCase() === "true";
 
 // check if item is on preOrder and notify user if so.
 if (preOrder) {
