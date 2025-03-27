1146b46af598b6c5fd7c145138589a943852a6e3
Cheryl Lee <clee0937@gmail.com>
2025-03-27 17:23:20 +0000

    added conditional statement to check if item is on pre-order

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import products from "./products";
 
-const productName: string = "tote bag";
+const productName: string = "shirt";
 
 // find the matching item from the inventory
 const product = products.find((item) => item.name === productName);
+
+/* Alternative - returns an array so we use [0] to select the first result.
+const product = products.filter((item) => item.name === productName)[0]; */
+
+console.log(`Found product ${JSON.stringify(product)}`);
+
+const preOrder = product?.preOrder.toLowerCase() === "true";
+
+// check if item is on preOrder and notify user if so.
+if (preOrder) {
+  console.log("We'll notify you once the item has been despatched");
+}
